849d2d12b2d285aa3f9a50ff4cbfcf4d514b8ac6
Andrew Dmitriev <dryundel21@gmail.com>
2021-06-23 12:14:39 +0300

    доделано

--- app/src/main/java/com/example/mortgagecalculation/SecActivity.java
+++ app/src/main/java/com/example/mortgagecalculation/SecActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SecActivity extends Activity {
     double everymonth_bet;
@@ -30,29 +31,37 @@ public class SecActivity extends Activity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                try {
+                    double percent = Double.parseDouble(procent);
+                    double sum = Double.parseDouble(String.valueOf(ed.getText()));
+                    double years = Double.parseDouble(String.valueOf(ed1.getText()));
 
-                double percent = Double.parseDouble(procent);
-                double sum = Double.parseDouble(String.valueOf(ed.getText()));
-                double years = Double.parseDouble(String.valueOf(ed1.getText()));
+                    everymonth_bet = percent / 12 / 100;
+                    total = Math.pow((1 + everymonth_bet), years * 12);
+                    everymonth_pay = sum * everymonth_bet * total / (total - 1);
+                    String ev_pay = String.format("%.3f", everymonth_pay);
+                    overpayenments = everymonth_pay * (years * 12) - sum;
+                    String ovpay = String.format("%.3f", overpayenments);
+                    all_money = everymonth_pay * (years * 12);
+                    String all = String.format("%.3f", all_money);
 
-                everymonth_bet = percent/12/100;
-                total = Math.pow((1+everymonth_bet), years*12);
-                everymonth_pay = sum * everymonth_bet * total / (total-1);
-                String ev_pay = String.format("%.3f",everymonth_pay);
-                overpayenments = everymonth_pay * (years*12) - sum;
-                String ovpay = String.format("%.3f",overpayenments);
-                all_money = everymonth_pay*(years*12);
-                String all = String.format("%.3f",all_money);
+                    System.out.println(everymonth_bet + "\n" + total + "\n" + everymonth_pay + "\n" + overpayenments + "\n" + all_money);
 
-                System.out.println(everymonth_bet+"\n"+ total+"\n"+everymonth_pay+"\n"+overpayenments+"\n"+ all_money);
+                    Intent intent = new Intent(SecActivity.this, ThirdActivity.class);
+                    intent.putExtra("Sum", String.valueOf(Math.round(sum)));
+                    intent.putExtra("All", all);
+                    intent.putExtra("Pay", ev_pay);
+                    intent.putExtra("Overpay", ovpay);
+                    intent.putExtra("years", String.valueOf(Math.round(years)));
+                    startActivity(intent);
 
-                Intent intent = new Intent(SecActivity.this, ThirdActivity.class);
-                intent.putExtra("Sum", String.valueOf(Math.round(sum)));
-                intent.putExtra("All", all);
-                intent.putExtra("Pay", ev_pay);
-                intent.putExtra("Overpay", ovpay);
-                intent.putExtra("years", String.valueOf(Math.round(years)));
-                startActivity(intent);
+                }
+                catch (Exception e){
+                        Toast toast = Toast.makeText(getApplicationContext(),
+                                "Введите данные", Toast.LENGTH_SHORT);
+                        toast.show();
+
+                }
             }
         });
     }
--- app/src/main/java/com/example/mortgagecalculation/ThirdActivity.java
+++ app/src/main/java/com/example/mortgagecalculation/ThirdActivity.java
@@ -1,6 +1,7 @@
 package com.example.mortgagecalculation;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -30,6 +31,8 @@ public class ThirdActivity extends Activity {
                 Toast toast = Toast.makeText(getApplicationContext(),
                         "Это программу сделали Дмитриев А.Д. и Погорелов Е.А.", Toast.LENGTH_LONG);
                 toast.show();
+                Intent intent = new Intent(ThirdActivity.this, MainActivity.class);
+                startActivity(intent);
             }
         });
     }
